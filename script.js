bcc39979f68e4b26e851dc62f970e8f577589b17
Matthew van Heerden <matt.v.h152342@gmail.com>
2026-04-22 19:28:25 +0200

    Create hover() and getGridSize() functions

--- script.js
+++ script.js
@@ -19,4 +19,33 @@ function createGrid(gridSize) {
     }
 }
 
+function hover() {
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach(cell => {
+        cell.addEventListener("mouseenter", (element) => {
+            cell.setAttribute("style", "margin: 0; padding: 0; width: 30px; height: 30px; border: 1px solid; background-color: gray; display: flex; flex: 0 1 auto;");
+            cell.classList.add("hovered");
+        });
+        cell.addEventListener("mouseleave", (element) => {
+            cell.setAttribute("style", "margin: 0; padding: 0; width: 30px; height: 30px; background-color: black; border: 1px solid black ; display: flex; flex: 0 1 auto;")
+        });
+    });
+}
+
+function getGridSize() {
+    const button = document.querySelector(".gridButton");
+    button.addEventListener('click', () => {
+        do {
+            size = Number(prompt("Enter new grid size: "));
+        } while (size > 64 || !Number(size));
+
+        const cells = document.querySelectorAll(".cell");
+        cells.forEach((element) => element.remove());
+        createGrid(size);
+        hover();
+    });
+}
+
+getGridSize();
 createGrid(gridSize);
+hover();
